9ca537eb32bfb26172a0bb39c41bc1814be3b92b
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-08-01 16:30:29 -0500

    created file for sidebar links

---
+++ constants/index.ts
@@ -0,0 +1,12 @@
+export const sidebarLinks = [
+    {
+        route: '/profile', 
+        label: 'Profile', 
+        imgURL: 'icons/microphone.svg'
+    }, 
+    {
+        route: '/home', 
+        label: 'Home', 
+        imgURL: 'icons/home.svg'
+    }
+]
