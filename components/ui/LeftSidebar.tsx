8fc2a110ce5a34118d73a4fd4ef8f86de3dbad38
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-08-01 17:34:32 -0500

    current page functionality

--- components/ui/LeftSidebar.tsx
+++ components/ui/LeftSidebar.tsx
@@ -1,9 +1,15 @@
+'use client'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
 import { sidebarLinks } from '@/constants'
+import { usePathname, useRouter } from 'next/navigation'
+import { cn } from '@/lib/utils'
 
 const LeftSidebar = () => {
+    const pathname = usePathname() 
+    const router = useRouter()
   return (
     <section className='left_sidebar'>
         <nav className='flex flex-col gap-6'>
@@ -13,11 +19,15 @@ const LeftSidebar = () => {
                 <h1 className='text-24 font-extrabold text-white max-lg:hidden'>PodAI</h1>
             </Link>
 
-            {sidebarLinks.map(({ route, label }) => {
+            {sidebarLinks.map(({ route, label, imgURL }) => {
+                const isActive = pathname === route || pathname.startsWith(`${route}/`);
 
-                return <Link href={route}>
-                    { label }
-                </Link>
+                return <Link href={route} key={label} className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
+                    'bg-nav-focus border-r-4 border-orange-1': isActive
+                  })}>
+                    <Image src={imgURL} alt={label} width={24} height={24} />
+                    <p>{label}</p>
+                  </Link>
             })}
         </nav>
     </section>
