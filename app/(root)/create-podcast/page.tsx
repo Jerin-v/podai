2755892ff7b08d55c20f4bb5438b2b3d24c2ffb4
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-08-02 13:14:12 -0500

    Update page titles and add mobile navigation component

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const CreatePodcast = () => {
   return (
-    <div>CreatePodcast</div>
+    <div>
+      <h1 className='text-20 font-bold text-white-1'>Create Podcast</h1>
+    </div>
   )
 }
 
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import LeftSidebar from "@/components/ui/LeftSidebar";
+import RightSidebar from "@/components/ui/RightSidebar";
+import Image from "next/image";
+import MobileNav from "@/components/ui/MobileNav";
 
 export default function RootLayout({
   children,
@@ -7,11 +10,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-        <main>
+    <div className="relative flex flex-col">
+        <main className="relative flex bg-black-3">
             <LeftSidebar />
-            {children}
-            <p className="text-white-1">RIGHT SIDEBAR</p>
+            
+            <section className="flex min-h-screen
+            flex-1 flex-col px-4 sm:px-14">
+              <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+                <div className="flex h-16 items-center justify-between md:hidden">
+                  <Image 
+                    src='/icons/logo.svg'
+                    width={30}
+                    height={30}
+                    alt="menu icon"/>
+                  <MobileNav />
+                </div>
+                <div className="flex flex-col md:pb-14">
+                  Toaster (notification popups)
+                  { children }
+                </div>
+              </div>
+            </section>
+
+            <RightSidebar />
         </main>
     </div>
   );
