d523ef5200da5610d78c4584bbfd103c024ac1d3
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-08-07 14:22:42 -0500

    convex setup steps 5-9

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,26 @@
+"use client";
+
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+
+
+
 
 
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+        <div className="flex min-h-screen flex-col items-center justify-between p-24
+        text-white-1">
+          {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+        </div>
+
         <Button className='text-white-1 bg-orange-1'>Button</Button>
       </section>
     </div>
