eb96422c072355e23e606e8d5e4f301f8b931547
Jerin Vandannoor <108553514+Jerin-v@users.noreply.github.com.>
2024-08-07 14:33:18 -0500

    create podcast card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,8 @@ import React from 'react'
 import { Button } from "@/components/ui/button"
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { podcastData } from '@/constants';
+import PodcastCard from '@/components/PodcastCard';
 
 
 
@@ -20,6 +22,15 @@ const Home = () => {
         text-white-1">
           {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
         </div>
+        {podcastData.map(({id, title, description, imgURL}) => (
+          <PodcastCard
+            key={id}
+            imgUrl={imgURL}
+            title={title}
+            description={description}
+            podcastId={id}
+            />
+        ))}
 
         <Button className='text-white-1 bg-orange-1'>Button</Button>
       </section>
---
+++ components/PodcastCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PodcastCard = ({
+    imgUrl, title, description, podcastId
+}: {
+    imgUrl: string;
+    title: string;
+    description: string;
+    podcastId: number;
+}) => {
+  return (
+    <div>PodcastCard</div>
+  )
+}
+
+export default PodcastCard
